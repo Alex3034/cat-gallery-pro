dcf47c8cccb1544e2e6a427a702d90ede4c600dc
Alejandro <alex1348wk@gmail.com>
2026-02-22 17:41:01 +0100

    chore: setup Tailwind CSS with PostCSS and configure index.css

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
 export default function Home() {
     return (
-        <div>
+        <div className="text-center text-3xl text-blue-600 mt-10 font-bold">
             <h1>Bienvenido a Cat Gallery Pro</h1>
             <p>Explora una galería de gatos adorables y guarda tus favoritos.</p>
         </div>
