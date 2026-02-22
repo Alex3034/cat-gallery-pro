61eebffeb53997134123d9e39fa475f7dacba022
Alejandro <alex1348wk@gmail.com>
2026-02-22 15:42:28 +0100

    Initial commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,8 @@
+export default function Home() {
+    return (
+        <div>
+            <h1>Bienvenido a Cat Gallery Pro</h1>
+            <p>Explora una galería de gatos adorables y guarda tus favoritos.</p>
+        </div>
+    );
+}
