f63c4d32a05e6ea54bc803c647cd4fc52d25d97b
Alejandro <alex1348wk@gmail.com>
2026-03-08 19:30:38 +0100

    chore: agregar Tailwind vía CDN y simplificar deploy para GitHub Pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,17 @@
+// main.jsx
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { FavoriteProvider } from './context/FavoritesContext.jsx'
+import { HashRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <FavoriteProvider>
-      <App />
+      <HashRouter>
+        <App />
+      </HashRouter>
     </FavoriteProvider>
   </StrictMode>,
 )
