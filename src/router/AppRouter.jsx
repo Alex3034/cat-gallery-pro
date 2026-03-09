97b194882585ae03d3ae48c8f1b9d10c2e0cdec2
Alejandro <alex1348wk@gmail.com>
2026-03-09 20:45:31 +0100

    fix: remove BrowserRouter from AppRouter to avoid nested router error

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,14 +1,12 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Home from '../pages/Home';
 import Favorites from '../pages/Favorites';
 
 export default function AppRouter() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/favorites" element={<Favorites />} />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/favorites" element={<Favorites />} />
+    </Routes>
   );
 }
